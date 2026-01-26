310db112637b147f4c7ae1d8b28b97d638a6194f
Aof Krajangpho <198059016+AofKraj11nashi@users.noreply.github.com>
2026-01-26 12:11:10 +0000

    Refactor item price calculation and remove comment

--- Week 2 - Activity/lab.21.py
+++ Week 2 - Activity/lab.21.py
@@ -1,5 +1,8 @@
+
+# Racist in my blood
 item_price = float(input("Enter your item price:"))
 item_tax = 8.5
 total_price = item_price + item_tax
 print("Tax on the item is",item_tax)
+
 print("Total item price is",total_price)
